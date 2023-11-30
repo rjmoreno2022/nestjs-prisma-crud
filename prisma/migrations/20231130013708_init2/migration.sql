cf0955a9fa3d0d86890a75ab8a187f508557c359
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2023-11-29 21:55:48 -0400

    Finished

---
+++ prisma/migrations/20231130013708_init2/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - Made the column `description` on table `Task` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
+INSERT INTO "new_Task" ("description", "id", "title") SELECT "description", "id", "title" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
